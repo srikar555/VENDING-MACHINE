2479ed6e975131fe3bc2f446faab0a6f8bfe0cbb
srikar555 <64193247+srikar555@users.noreply.github.com>
2023-11-17 12:22:15 +0530

    Update TB.v

--- TB.v
+++ TB.v
@@ -1,61 +1,65 @@
-// Code your testbench here
-// or browse Examples
 `timescale 1ns/1ps
-module TB;
-  reg clk,reset,cancel;
-  reg [4:0] money;
-  reg [5:0] sel_item;
-  wire [4:0] change; //change in 5,10,20 denomination
-  wire [5:0]item_out; // upto 64 items
- 
-  vending_machine DUT(.clk(clk),
-                      .reset(reset),
-                      .money(money),
-                      .cancel(cancel),
-                      .sel_item(sel_item),
-                      .change(change),
-                      
-                      .item_out(item_out));
- 
-   initial begin
-    clk=0;
-    reset=1;
-    cancel=0;
-    money=4'd0;
-    sel_item=6'd0;
-     $dumpfile("vending_machine.vcd");
-     $dumpvars(0,TB);
-  end
-  always #15 clk=~clk;
+
+module testbench;
+
+  // Inputs
+  reg clk;
+  reg reset;
+  reg [1:0] money;
+  reg [7:0] sel_item;
+
+  // Outputs
+  wire [1:0] change;
+  wire [7:0] item_out;
+
+  // Instantiate the module under test
+  vending_machine uut(
+    .clk(clk),
+    .reset(reset),
+    .money(money),
+    .sel_item(sel_item),
+    .change(change),
+    .item_out(item_out)
+  );
+
+  // Clock generation
+  always #5 clk = ~clk;
+
+  // Initial values
   initial begin
-    #1 reset =0;
-    #14 money=4'd5;
-    #15 money = 4'd0;
-   
-    #15 money=4'd5;
-     
-    #15 money = 4'd0;
-    
-    #15 money=4'd5;
-    #3 sel_item = 6'd1;
-    #15 money = 4'd0;
-    #45 money=4'd5;
-     
-    #15 money = 4'd0;
-    #15 money=4'd5;
-    #15 money = 4'd0;
-    #15 money = 4'd0;
-    #15 money=4'd5;
-    #15 money = 4'd0;
-    #15 money=4'd5;
-    #15 money = 4'd0;
-    #15 money=4'd5;
-    #15 money = 4'd0;
-    #15 money=4'd5;
-    #15 money = 4'd0;
+    $dumpfile("waveform.vcd");
+    $dumpvars(0, testbench);
 
+    clk = 0;
+    reset = 1;
+    money = 0;
+    sel_item = 0;
+    #10 reset = 0; // De-assert reset after 10 time units
 
-    #60 $finish;
+    // Test scenario 1: Insert money, select item
+    // Example scenario - You can modify and add your test cases here
+    #14 money = 2'b01; // Insert 5 units
+    #10 money=0;
+    #10 sel_item = 8'hA1;
+    #6 sel_item = 8'h00;// Selec news_paper
+       #14 money = 2'b01; // Insert 5 units
+    #10 money=0;
+    #10 sel_item = 8'hA1;
+    #6 sel_item = 8'h00;// Selec news_paper
+    #100; // Add more test cases or delays as needed
+    // End of test scenario
+
+    // You can add more test scenarios here by changing inputs and observing outputs
+    // Ensure enough simulation time to observe the behavior of the module
+
+    // Finish simulation after a certain time
+    #500;
+    $finish;
   end
+
+  // Display outputs during simulation
+  always @(change or item_out) begin
+    $display("Change: %b, Item Out: %h", change, item_out);
+  end
+
 endmodule
-    
